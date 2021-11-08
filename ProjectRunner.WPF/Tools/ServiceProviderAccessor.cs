02582a6594822942cd94688319cd18ea53293e5e
elitonluiz1989 <elitonluiz1989@gmail.com>
2021-11-07 20:58:41 -0400

    Created global accessor for service provider

---
+++ ProjectRunner.WPF/Tools/ServiceProviderAccessor.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ProjectRunner.WPF.Tools
+{
+    public static class ServiceProviderAccessor
+    {
+        private static IServiceProvider _serviceProvider;
+
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public static T GetRequiredService<T>()
+        {
+            return _serviceProvider.GetRequiredService<T>();
+        }
+    }
+}
